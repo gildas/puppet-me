46f3574cefdc0748e7ccc08340f783e107346f9d
Gildas Cherruel <gildas@breizh.org>
2014-01-28 01:42:55 +0900

    Allowed the installer to interact with the CLI

--- install/osx-client.sh
+++ install/osx-client.sh
@@ -87,7 +87,7 @@ function install_dmg()
   verbose " NOTE: You might have to enter your password to allow that package to be installed!"
   package=$(find ${mount} -name '*.pkg' -mindepth 1 -maxdepth 1)
   verbose "  Package: ${package}"
-  sudo installer -pkg ${package} -target / > /dev/null
+  sudo installer -pkg ${package} -target /
 
   verbose "Unmounting ${target}"
   hdiutil eject ${mount} > /dev/null
