21af8b02f52d15c89789b3efa662cc335d7d63ec
Gildas Cherruel <gildas.cherruel@inin.com>
2014-02-06 00:15:58 -0800

    msi is now downloading, stuck when running the installer.

--- install/install.ps1
+++ install/install.ps1
@@ -1,19 +1,28 @@
-function Install-Package
+﻿param(
+  [Parameter(Mandatory=$true)][string] $PuppetMaster,
+  [Parameter(Mandatory=$true)][string] $ClientCert,
+  [string] $Environment='production'
+)
+
+function Curl-File(
+  [string] $Module,
+  [string] $Version,
+  [string] $Source)
 {
-  param
-  (
-    [string]$Source,
-    [string]$Version,
-    [string]$Package
-  )
-  
-  $package = '1234.msi'
-  $url = $Source + $package
-  $destination = Join-Path $env:TEMP $package
-  Write-Host "Downloading $url to $file"
-  ((new-object net.webclient).DownloadFile($url, $destination)
+  $downloader=New-Object Net.Webclient
+
+  $result=$downloader.DownloadString($Source).Split("`n") | where { $_ -match "$Module-\d\.\d\.\d\.msi" } | Select-Object -Last 1 | %{ $_ -match '.*href="(?<filename>[^"]*)".*' }
+  $filename=$matches['filename']
+
+  $destination = Join-Path (Join-Path $env:USERPROFILE "DownLoads") $filename
+
+  Write-Host "Downloading $Source/$filename into $destination"
+  $downloader.DownloadFile("$Source/$filename", $destination)
+
+  return $destination
 }
 
-# install PuppetLabs from their website
-Install-Package -Source 'http://downloads.puppetlabs.com/windows/' -Version '*' -Package facter
+$destination = Curl-File -Module "Puppet" -Version "*" -Source "http://downloads.puppetlabs.com/windows/"
 
+Write-Host "Installing $destination"
+Start-Process -File "msiexec.exe" -arg "/qn /i $destination /l*v install-puppet.log PUPPET_MASTER_SERVER=$PuppetMaster PUPPET_AGENT_CERTNAME=$ClientCert" -PassThru | Wait-Process
