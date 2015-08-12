6fb4695df4e1274c7af5477ecf5629906dc67df4
Gildas Cherruel <gildas.cherruel@inin.com>
2015-08-12 18:54:52 +0900

    Installing chocolatey automatically

--- install/win-client-8.1.cmd
+++ install/win-client-8.1.cmd
@@ -5,34 +5,43 @@ title DaaS setup
 
 set CURRENT_DIR=%~dp0%
 
-echo Checking Explorer...
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Get-Command 'explorer' -TotalCount 1 -ErrorAction SilentlyContinue | ForEach-Object { $_.Path }"
-echo Errorlevel: %ERRORLEVEL%
-if errorlevel 0 echo installed
-if errorlevel 1 echo not installed
+goto main
+
+:: functions
+:Download
+echo Downloading %~1 into %~2
+for /f "useback tokens=* delims=/" %%a in ("%~1") do set filename=%%a
+echo Filename: %filename%
+::%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy ByPass -Command "((new-object net.webclient).DownloadFile('https://chocolatey.org/install.ps1', '%TEMP%\install.ps1'))"
+
+goto :EOF
+
+:main
 
 echo Checking Chocolatey...
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Get-Command 'chocolatey' -TotalCount 1 -ErrorAction SilentlyContinue | ForEach-Object { $_.Path }"
-echo Errorlevel: %ERRORLEVEL%
-if errorlevel 0 echo installed
-if errorlevel 1 echo not installed
-
-echo Checking Chocolatey...
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Get-Command 'chocolatey' -TotalCount 1 -ErrorAction SilentlyContinue | ForEach-Object { $_.Path }"
-if errorlevel 1 goto installing_chocolatey
-goto installed_chocolatey
+where.exe /q chocolatey.exe
+if %ERRORLEVEL% EQU 0 goto installed_chocolatey
+if EXIST %ALLUSERSPROFILE%\chocolatey\bin\chocolatey.exe (
+  SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+  goto installed_chocolatey
+)
+goto installing_chocolatey
 
 :installing_chocolatey
 title Installing Chocolatey...
-echo Installing Chocolatey...
+echo Downloading Chocolatey Installer...
+::call :Download "https://chocolatey.org/install.ps1" %TEMP%
 %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy ByPass -Command "((new-object net.webclient).DownloadFile('https://chocolatey.org/install.ps1', '%TEMP%\install.ps1'))"
 if errorlevel 1 goto installing_chocolatey
+echo Installing Chocolatey...
 %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy ByPass -Command "& '%TEMP%\install.ps1' %*"
 if errorlevel 1 goto installing_chocolatey
+SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 goto installed_chocolatey
 
 :installed_chocolatey
 echo Chocolatey is installed
+call :Download "https://chocolatey.org/install.ps1" %TEMP%
 
 goto success
 
