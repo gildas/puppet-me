f6efb759de2beec322dfe0c6f7f29800f8218e8e
Gildas Cherruel <gildas.cherruel@inin.com>
2015-08-14 20:53:03 +0900

    Fixed download URL for Virtualbox Extension Pack

--- install/Install-VirtualboxExtensionPack.ps1
+++ install/Install-VirtualboxExtensionPack.ps1
@@ -4,8 +4,10 @@ $vboxManage=Join-Path $env:VBOX_MSI_INSTALL_PATH 'VBoxManage.Exe'
 
 $vboxVersion= & $vboxManage --version
 $vboxExtensionPack="Oracle_VM_VirtualBox_Extension_Pack-$($vboxVersion -replace 'r','-').vbox-extpack"
-$url="${url}/$($vboxVersion -replace 'r','/')/${vboxExtensionPack}"
+$url="${url}/$($vboxVersion -replace 'r.*','')/${vboxExtensionPack}"
 
+Write-Output "Downloading $url"
 Invoke-webRequest -Uri $url -OutFile (Join-Path $env:TEMP $vboxExtensionPack)
 
+Write-Output "Installing Extension pack version ${vboxVersion}"
 & $vboxManage extpack install --replace (Join-Path $env:TEMP $vboxExtensionPack) 2>&1
